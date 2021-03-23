df2ce45357686f432d1cd8b85f1b5505558a98f5
LckyLke <lukefriedrichs@gmail.com>
2021-03-23 21:39:48 +0100

    ok

---
+++ src/LandingPage/pages/Startpage/Startpage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Startpage.sass"
+import NavHeader from "../../../Shared/NavHeader/NavHeader"
+
+const Startpage = () => {
+    return (
+        <div className="main">
+            <NavHeader/>
+        </div>
+    )
+}
+
+export default Startpage
---
+++ src/Shared/NavHeader/NavHeader.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./NavHeader.sass"
+
+const NavHeader = () => {
+    return (
+        <nav className="mainNav">
+            <div className="logoContainer">
+                <div className="lineOne"></div>
+                <div className="lineTwo"></div>
+                <div className="lineBot"></div>
+            </div>
+            <div>Placeh</div>
+            <div>Placeh</div>
+            <div>Placeh</div>
+            <div>Placeh</div>
+        </nav>
+    )
+}
+
+export default NavHeader
