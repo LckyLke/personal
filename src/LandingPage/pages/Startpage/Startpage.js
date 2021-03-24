20a6262b9f028a71f202e16b8f3b4f6f894aade3
LckyLke <lukefriedrichs@gmail.com>
2021-03-24 22:23:14 +0100

    Added SVG and started animating the svg using css (sass) keyframe animations

--- src/LandingPage/pages/Startpage/Startpage.js
+++ src/LandingPage/pages/Startpage/Startpage.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import "./Startpage.sass"
 import NavHeader from "../../../Shared/NavHeader/NavHeader"
+import AnimPcBoy from "../../components/AnimPcBoy"
 
 const Startpage = () => {
     return (
         <div className="main">
+
             <NavHeader/>
+            <AnimPcBoy/>
+            
         </div>
     )
 }
--- src/Shared/NavHeader/NavHeader.js
+++ src/Shared/NavHeader/NavHeader.js
@@ -1,20 +1,43 @@
-import React from 'react'
-import "./NavHeader.sass"
+import React from "react";
+import "./NavHeader.sass";
 
 const NavHeader = () => {
-    return (
-        <nav className="mainNav">
-            <div className="logoContainer">
-                <div className="lineOne"></div>
-                <div className="lineTwo"></div>
-                <div className="lineBot"></div>
-            </div>
-            <div>Placeh</div>
-            <div>Placeh</div>
-            <div>Placeh</div>
-            <div>Placeh</div>
-        </nav>
-    )
-}
+  return (
+    <nav className="mainNav">
+      <svg width="100" height="100">
+        <line
+          x1="20"
+          x2="20"
+          y1="10"
+          y2="100%"
+          stroke="white"
+          strokeWidth="5"
+        />
+        <line
+          x1="30"
+          x2="30"
+          y1="10"
+          y2="100%"
+          stroke="white"
+          strokeWidth="5"
+        />
+        <line
+          x1="30"
+          x2="70%"
+          y1="97.5"
+          y2="97.5"
+          stroke="white"
+          strokeWidth="5"
+        />
+      </svg>
+      <div className="spaceNav">
+        <div>Placeh</div>
+        <div>Placeh</div>
+        <div>Placeh</div>
+        <div>Placeh</div>
+      </div>
+    </nav>
+  );
+};
 
-export default NavHeader
+export default NavHeader;
