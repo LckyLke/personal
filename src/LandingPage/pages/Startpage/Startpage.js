4352a49d18cdd513a64dfe1f5b3b63b53db11bd6
LckyLke <lukefriedrichs@gmail.com>
2021-03-26 00:11:05 +0100

    New Svg - New Design - New Logo - New Animations

--- src/LandingPage/pages/Startpage/Startpage.js
+++ src/LandingPage/pages/Startpage/Startpage.js
@@ -2,12 +2,23 @@ import React from 'react'
 import "./Startpage.sass"
 import NavHeader from "../../../Shared/NavHeader/NavHeader"
 import AnimPcBoy from "../../components/AnimPcBoy"
+import Logo from "../../components/Logo"
 
 const Startpage = () => {
+
+    const destructureStringAndAssingClassForChar = (string, classNamee) => {
+       return string.split("").map((char) => <span key={Math.random()} className={classNamee}>{char}</span>)
+    } 
+
     return (
         <div className="main">
 
-            <NavHeader/>
+            {/*<NavHeader/>*/}
+            <Logo/>
+            <div className="AboutTextField">
+                <h1>{destructureStringAndAssingClassForChar("I'm Luke.", "char")}</h1>
+                <span className="subText">A fullstack developer and web designer.</span>
+            </div>
             <AnimPcBoy/>
             
         </div>
